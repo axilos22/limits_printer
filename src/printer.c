a78af43ddf972a841981985a39c45f11ea8ae480
Axilos <zenos_22@hotmail.com>
2022-06-12 16:27:29 +0200

    Add width trick
    * Now uses width trick instead of hardcoded width
    * Format code
    * Naming improvement

--- src/printer.c
+++ src/printer.c
@@ -8,42 +8,43 @@
 
 #include "printer.h"
 
-const int default_width = 30;
+const int field_width = 30;
 const int field_number = 3;
-const char separator_char = '-';
+const char separator = '-';
 
 // Private functions
 void print_int_boundaries(const char *type, const long long int lower_bound, const long long int upper_bound)
 {
-    printf("%30s | %30lli | %30lli \n", type, lower_bound, upper_bound);
+    printf("%*s | %*lli | %*lli \n", field_width, type, field_width, lower_bound, field_width, upper_bound);
 }
 
 void print_unsigned_int_boudaries(const char *type, const unsigned long long int upper_bound)
 {
-    printf("%30s | %30llu | %30llu \n", type, 0ull, upper_bound);
+    printf("%*s | %*llu | %*llu \n", field_width, type, field_width, 0ull, field_width, upper_bound);
 }
 
 void print_float_boundaries(const char *type, const double lower_bound, const double upper_bound)
 {
-    printf("%30s | %30Le | %30Le \n", type, lower_bound, upper_bound);
+    printf("%*s | %*Le | %*Le \n", field_width, type, field_width, lower_bound, field_width, upper_bound);
 }
 
 void print_long_double_boundaries(const char *type, const long double lower_bound, const long double upper_bound)
 {
-    printf("%30s | %30Le | %30Le \n", type, lower_bound, upper_bound);
+    printf("%*s | %*Le | %*Le \n", field_width, type, field_width, lower_bound, field_width, upper_bound);
 }
 
-void print_separator()
+void print_separator_line()
 {
-    for (int rank = 0; rank < field_number * default_width; rank++)
+    for (int rank = 0; rank < field_number * field_width; rank++)
     {
-        printf("%c", separator_char);
+        printf("%c", separator);
     }
     printf("\n");
 }
 
-void print_header() {
-    printf("%30s | %30s |%30s \n", "Type name", "Lower bound", "Upper bound");
+void print_header()
+{
+    printf("%*s | %*s |%*s \n", field_width, "Type name", field_width, "Lower bound", field_width, "Upper bound");
 }
 
 // Public functions
@@ -66,34 +67,37 @@ void print_usage()
     printf("\t %s\n", "t : Runs the test sequence");
 }
 
-void print_integer_limits() {
+void print_integer_limits()
+{
     printf("Integer types\n");
     print_int_boundaries("SIGNED CHAR", SCHAR_MIN, SCHAR_MAX);
     print_unsigned_int_boudaries("UNSIGNED CHAR", UCHAR_MAX);
-    print_separator();
+    print_separator_line();
     print_int_boundaries("SIGNED SHORT", SHRT_MIN, SHRT_MAX);
     print_unsigned_int_boudaries("UNSIGNED SHORT", USHRT_MAX);
-    print_separator();
+    print_separator_line();
     print_int_boundaries("SIGNED INTEGER", INT_MIN, INT_MAX);
     print_unsigned_int_boudaries("UNSIGNED INTEGER", UINT_MAX);
-    print_separator();
+    print_separator_line();
     print_int_boundaries("SIGNED LONG", LONG_MIN, LONG_MAX);
     print_unsigned_int_boudaries("UNSIGNED LONG", ULONG_MAX);
-    print_separator();
+    print_separator_line();
     print_int_boundaries("SIGNED LONG LONG", LLONG_MIN, LLONG_MAX);
     print_unsigned_int_boudaries("UNSIGNED LONG LONG", ULLONG_MAX);
-    print_separator();
+    print_separator_line();
 }
 
-void print_float_limits() {
+void print_float_limits()
+{
     printf("Floating point types\n");
     print_float_boundaries("FLOAT", FLT_MIN, FLT_MAX);
     print_float_boundaries("DOUBLE", DBL_MIN, DBL_MAX);
     print_long_double_boundaries("LONG DOUBLE", LDBL_MIN, LDBL_MAX);
-    print_separator();
+    print_separator_line();
 }
 
-void print_all_limits() {
+void print_all_limits()
+{
     print_header();
     print_integer_limits();
     print_float_limits();
--- src/printer.h
+++ src/printer.h
@@ -2,7 +2,7 @@
 #define PRINTER_H
 
 void print_usage();
-void print_separator();
+void print_separator_line();
 void print_float_limits();
 void print_integer_limits();
 void print_all_limits();
